9acae4d0c5ba3b19eccc3a6d292233e5e7972b33
mchlangu <wsbungela@gmail.com>
2025-09-11 13:40:07 +0200

    Set of project skeleton completed. Angular + Bootstrap + Spring boot + PostgreSQL

---
+++ wicommerce-backend/src/main/java/com/willioncommerce/wicommerce_backend/WicommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.willioncommerce.wicommerce_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WicommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WicommerceBackendApplication.class, args);
+	}
+
+}
---
+++ wicommerce-backend/src/test/java/com/willioncommerce/wicommerce_backend/WicommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willioncommerce.wicommerce_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WicommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
