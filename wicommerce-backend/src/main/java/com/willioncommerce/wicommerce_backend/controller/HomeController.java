4b2ea7baf0fa15ec05f7c64725e3af7d17da8145
mchlangu <wsbungela@gmail.com>
2025-09-13 04:22:13 +0200

    Global CORS Config

---
+++ wicommerce-backend/src/main/java/com/willioncommerce/wicommerce_backend/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.willioncommerce.wicommerce_backend.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HomeController {
+
+    @RequestMapping("/")
+    public String welcomeHome(){
+        return "Welcome home";
+    }
+
+
+}
